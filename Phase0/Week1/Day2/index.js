015324de28776484d84651906d1986c4e6750d97
Gifania Sofia Lestari <gifaniasofia@gmail.com>
2020-11-24 17:42:22 +0700

    Add files via upload

---
+++ Phase0/Week1/Day2/index.js
@@ -0,0 +1,32 @@
+/*
+MIND MAPPING
+============
+Deskripsikan nilai pi. Masukkan nilai jariJari (saya memasukkan nilai jariJari adalah 7). Kemudian, hitung nilai keliling lingkaran sesuai rumusnya yaitu 2*pi*jariJari.
+
+ALGORITMA
+=========
+1. Deklarasikan variabel bernama pi dan masukkan nilai pi
+2. Siapkan variabel bernama jariJari dan masukkan sebuah nilai ke dalam variabel jariJari
+3. Siapkan variabel bernama kelilingLingkaran untuk diisikan nanti
+4. Isikan hasil proses perhitungan perkalian antara 2, variabel pi, dan variabel jariJari, ke dalam variabel kelilingLingkaran
+5. Tampilkan nilai kelilingLingkaran
+
+PSEUDOCODE
+==========
+STORE pi AS Number WITH 3.14
+STORE jariJari AS Number WITH 7
+STORE kelilingLingkaran AS Number WITH ANY VALUE
+
+SET kelilingLingkaran WITH 2 TIMES pi TIMES jariJari
+
+DISPLAY kelilingLingkaran
+*/
+
+// Insert your code here
+var pi = Math.PI;
+var jariJari = 7;
+var kelilingLingkaran;
+
+kelilingLingkaran = 2 * pi * jariJari;
+
+console.log(kelilingLingkaran);
