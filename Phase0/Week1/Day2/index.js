fc22e2802cf9204ea5b311c5548a274dbf9c7c42
Gifania Sofia Lestari <gifaniasofia@gmail.com>
2020-11-24 17:40:38 +0700

    rename index.js Newton-Second-Law.js

---
+++ Phase0/Week1/Day2/index.js
@@ -0,0 +1,32 @@
+/*
+MIND MAPPING
+============
+Nilai massa dan nilai percepatan dikalikan dan disimpan dalam hasil yang disebut sebagai resultan gaya. Dengan nilai massa adalah 600 dan nilai percepatan adalah 2.
+
+ALGORITMA
+============
+1. Siapkan variabel bernama massa dan masukkan nilai yang telah ditentukan ke dalam variabel massa
+2. Siapkan variabel bernama percepatan dan masukkan nilai yang telah ditentukan ke dalam variabel percepatan
+3. Siapkan variabel bernama resultanGaya
+4. Lakukan proses perhitungan perkalian antara variabel massa dan percepatan, di mana hasil perhitungan tersebut diisikan ke dalam variabel resultanGaya
+5. Tampilkan nilai gaya
+
+PSEUDOCODE
+============
+STORE massa AS Number WITH 600
+STORE pecepatan AS Number WITH 2
+STORE resultanGaya AS Number WITH ANY VALUE
+
+SET resultanGaya WITH massa TIMES percepatan
+
+DISPLAY resultanGaya
+*/
+
+// Insert your code here
+var massa = 600;
+var percepatan = 2;
+var resultanGaya;
+
+resultanGaya = massa * percepatan;
+
+console.log(resultanGaya);
