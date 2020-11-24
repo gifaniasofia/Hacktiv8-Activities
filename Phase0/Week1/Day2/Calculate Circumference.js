9076caf72443a858c22488bd2b91317c4b4b3a95
Gifania Sofia Lestari <gifaniasofia@gmail.com>
2020-11-24 12:20:26 +0700

    Update Calculate Circumference.js

--- Phase0/Week1/Day2/Calculate Circumference.js
+++ Phase0/Week1/Day2/Calculate Circumference.js
@@ -1,1 +1,19 @@
-//
+// MIND MAPPING
+Asumsikan nilai pi adalah (22/7). Masukkan nilai jariJari, dan dalam hal ini saya menentukan nilai jariJari adalah 7. Kemudian, hitung nilai keliling sesuai rumusnya yaitu 2*pi*jariJari.
+
+// ALGORITMA
+1. Deklarasikan variabel bernama pi dengan nilai (22/7)
+2. Siapkan variabel bernama jariJari dan masukkan nilai ke dalam variabel jariJari
+3. Siapkan variabel bernama kelilingLingkaran untuk diisikan nanti
+4. Lakukan proses perhitungan perkalian antara 2, variabel pi, dan variabel jariJari. Kemudian update/isikan hasil perhitungan ke dalam variabel kelilingLingkaran
+5. Tampilkan nilai kelilingLingkaran
+
+
+// PSEUDOCODE
+STORE pi AS Number WITH (22/7)
+STORE jariJari AS Number WITH 7
+STORE kelilingLingkaran AS Number WITH ANY VALUE
+
+SET kelilingLingkaran WITH 2 TIMES pi TIMES jariJari
+
+DISPLAY kelilingLingkaran
